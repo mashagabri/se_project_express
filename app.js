ba6c8f919dc9f58fa4c14bf96a4b472a71405192
mashagabri <mashagabri@outlook.com>
2026-01-16 16:38:14 +0200

    fixed

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://www.mariazackwtwr.jumpingcrab.com"
   );
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
   next();
 });
 app.use(requestLogger);
